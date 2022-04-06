106102ad5df98c5b7af3670559219af5d9c7bab0
Manu Morante <manu@estadologico.com>
2022-04-06 21:40:44 +0200

    Render html from js

---
+++ src/posters.js
@@ -0,0 +1,45 @@
+export const POSTERS = [
+  {
+    title: 'Forrest Gump',
+    subtitle: 'By Robert Zemeckis',
+    slug: 'forrest-gump',
+    colors: { wrap: '#77b1c9aa', body: '#77b1c9', text: '#fff' },
+    html: "<div class='park-bench'><div class='plank plank-1'></div><div class='plank plank-2'></div><div class='plank plank-3'></div><div class='plank plank-4'></div><div class='leg leg-1'></div><div class='leg leg-2'></div><div class='leg leg-3'></div><div class='leg leg-4'></div><div class='side front'></div><div class='side back'></div><div class='side right'></div><div class='side left'></div><div class='side top'></div><div class='side bottom'></div></div>",
+  },
+  {
+    title: 'Home Alone',
+    subtitle: '',
+    slug: 'home-alone',
+    html: '',
+  },
+  {
+    title: 'Kill Bill',
+    subtitle: 'By Quentin Tarantino',
+    slug: 'kill-bill',
+    html: '<svg class="sword"><path d="M0,0h423c0,0-32.5,66.5-157,71.5H0V0z"></path></svg>',
+  },
+  {
+    title: 'Star Wars',
+    subtitle: 'By George Lucas',
+    slug: 'star-wars',
+    html: '<div class="space-ship"></div><div class="cube"><div class="death-ray"></div><div class="side front"></div><div class="side back"></div><div class="side right"></div><div class="side left"></div><div class="side top"></div><div class="side bottom"></div></div>',
+  },
+  {
+    title: 'The Matrix',
+    subtitle: 'By Wachowski Brothers',
+    slug: 'the-matrix',
+    html: '',
+  },
+  {
+    title: 'Titanic',
+    subtitle: '',
+    slug: 'titanic',
+    html: '',
+  },
+  {
+    title: 'Up',
+    subtitle: '',
+    slug: 'up',
+    html: '',
+  },
+]
--- src/posters/All.jsx
+++ src/posters/All.jsx
@@ -1,70 +1,28 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import ForrestGump from './ForrestGump'
-import HomeAlone from './HomeAlone'
-import KillBill from './KillBill'
-import StarWars from './StarWars'
-import TheMatrix from './TheMatrix'
-import Titanic from './Titanic'
-import Up from './Up'
+import { POSTERS } from '../posters'
 
-export default function All() {
-  const POSTERS = [
-    {
-      title: 'Forrest Gump',
-      slug: 'forrest-gump',
-      poster: <ForrestGump />,
-    },
-    {
-      title: 'Home Alone',
-      slug: 'home-alone',
-      poster: <HomeAlone />,
-    },
-    {
-      title: 'Kill Bill',
-      slug: 'kill-bill',
-      poster: <KillBill />,
-    },
-    {
-      title: 'Star Wars',
-      slug: 'star-wars',
-      poster: <StarWars />,
-    },
-    {
-      title: 'The Matrix',
-      slug: 'the-matrix',
-      poster: <TheMatrix />,
-    },
-    {
-      title: 'Titanic',
-      slug: 'titanic',
-      poster: <Titanic />,
-    },
-    {
-      title: 'Up',
-      slug: 'up',
-      poster: <Up />,
-    },
-  ]
-  useEffect(() => {
-    document.documentElement.style.setProperty('--bg-dark', '')
-    document.documentElement.style.setProperty('--bg', '')
-    document.documentElement.style.setProperty('--text', '')
-  }, [])
+function Poster({ poster }) {
+  // Poster wrapper classes
+  const classes = `${poster.slug} relative w-full max-w-[500px] aspect-poster transition-colors overflow-hidden`
+  const title = `<div class="title">${poster.title}</div>`
+  const subtitle = `<div class="subtitle">${poster.subtitle}</div>`
+  const html = title + subtitle + poster.html
 
   return (
-    <>
-      <div className='All mt-11 p-6 flex items-center justify-center flex-col gap-6 snap-y'>
-        {POSTERS.map(({ title, slug, poster }) => (
-          <Link
-            key={slug}
-            to={`/${slug}`}
-            title={title}
-            className={`${slug} snap-start relative w-full max-w-[500px] aspect-poster transition-colors overflow-hidden`}>
-            {poster}
-          </Link>
-        ))}
-      </div>
-    </>
+    <div
+      className={classes}
+      dangerouslySetInnerHTML={{
+        __html: html,
+      }}></div>
+  )
+}
+
+export default function All() {
+  return (
+    <div className='All mt-11 p-6 flex items-center justify-center flex-col gap-6 snap-y'>
+      {POSTERS.map((poster) => {
+        return <Poster key={poster.slug} poster={poster} />
+      })}
+    </div>
   )
 }
