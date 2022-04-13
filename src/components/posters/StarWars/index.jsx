6757e48f454063a91a5b9198aa89e5957df60c26
Manu Morante <manu@estadologico.com>
2022-04-13 22:02:39 +0200

    Posters css

--- src/components/posters/StarWars/index.jsx
+++ src/components/posters/StarWars/index.jsx
@@ -3,17 +3,10 @@ import './styles.css'
 
 export default function StarWars() {
   return (
-    <>
-      <div className='space-ship'></div>
+    <div className='death-star'>
       <div className='cube'>
         <div className='death-ray'></div>
-        <div className='side front'></div>
-        <div className='side back'></div>
-        <div className='side right'></div>
-        <div className='side left'></div>
-        <div className='side top'></div>
-        <div className='side bottom'></div>
       </div>
-    </>
+    </div>
   )
 }
