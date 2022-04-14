81af046b3edc65ece6f1a8298e84a29da81bc676
Manu Morante <manu@estadologico.com>
2022-04-14 17:41:00 +0200

    English

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -4,11 +4,9 @@ export default function Hello() {
   return (
     <div className='Hello md:max-w-[320px] h-screen sm:h-auto flex justify-center items-center p-8 sm:p-12 md:fixed md:top-10 md:left-0'>
       <span className='text-4xl md:text-2xl text-center md:text-left leading-snug'>
-        <span className='font-bold'>
-          Posters de películas al estilo{' '}
-          <span className='font-thin'>minimalista</span>{' '}
-        </span>
-        <span className='font-light'>creados y animados por código</span>
+        <span className='font-thin'>Minimalistic</span>{' '}
+        <span className='font-bold'>movie posters</span>{' '}
+        <span className='font-light'>created and animated by code</span>
       </span>
     </div>
   )
