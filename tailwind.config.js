4d41daef49e29c15bcf415eaaa866c9de55f15cd
Manu Morante <manu@estadologico.com>
2022-04-15 01:41:32 +0200

    Hal 9000 and jl funny values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@ module.exports = {
   content: ['./src/**/*.jsx', './index.html'],
   theme: {
     extend: {
+      screens: {
+        jl: '321px',
+      },
       aspectRatio: {
         poster: '2 / 3',
+        hal9000: '22 / 65',
       },
     },
   },
