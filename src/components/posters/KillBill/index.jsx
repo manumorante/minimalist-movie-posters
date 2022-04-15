9422a1fdc53c88586bef789c34fbd794492aafff
Manu Morante <manu@estadologico.com>
2022-04-15 11:50:57 +0200

    KillBill responsive

--- src/components/posters/KillBill/index.jsx
+++ src/components/posters/KillBill/index.jsx
@@ -4,6 +4,7 @@ import './styles.css'
 export default function KillBill() {
   return (
     <>
+      <div className="blood"></div>
       <svg className='sword'>
         <path d='M0,0h423c0,0-32.5,66.5-157,71.5H0V0z'></path>
       </svg>
